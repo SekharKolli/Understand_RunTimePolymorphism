f6c7cf3c79ec85de5a538e49b92bc17decc2c6c2
Sekhar Kolli <Sekhar.Kolli@gmail.com>
2019-01-13 19:56:29 -0500

    First examples demo'ng Runtime Polymorphism

---
+++ src/myRunTimePolymorphismTests/InheritanceWhichStmtsAreCorrect.java
@@ -0,0 +1,53 @@
+/*
+ * Purpose : Demonstrates how derived class variables behave/react when pointing to parent class variables
+ * Point : BaseClass or ParentClass can reference child objects and not vice-versa
+ *
+ * Date: 30-December-2018
+ */
+
+package myRunTimePolymorphismTests;
+
+class Parent {
+	// Some Member variables
+	// Some Methods
+
+} // EO Parent
+
+class ChildOne extends Parent {
+	// Some Member variables
+	// Some Methods
+
+} // EO ChildOne
+
+class ChildTwo extends Parent {
+	// Some Member variables
+	// Some Methods
+
+} // EO ChildTwo
+
+public class InheritanceWhichStmtsAreCorrect {
+
+	public static void main(String[] args) {
+
+		Parent p;
+		p = new Parent(); // Correct
+		p = new ChildOne(); // Correct
+		p = new ChildTwo(); // Correct
+
+		ChildOne o;
+		// o = new Parent(); // ** InCorrect ** CE: Type mismatch: cannot convert from
+		// Parent to ChildOne
+		o = new ChildOne();
+		// o = new ChildTwo(); // ** InCorrect ** CE: Type mismatch:
+
+		ChildTwo t;
+		// t = new Parent(); // ** InCorrect ** CE: Type mismatch: cannot convert from
+		// Parent to ChildTwo
+		// t = new ChildOne(); // ** InCorrect ** CE: Type mismatch:
+		t = new ChildTwo();
+
+		System.out.println("Program Execution Complete!!");
+
+	}
+
+}
---
+++ src/myRunTimePolymorphismTests/MyRunTimePolyTest.java
@@ -0,0 +1,48 @@
+/*
+ * Purpose : shows how runtime polymorphism works. Displays the behavior of methods and attributes
+ * 
+ *
+ * Date: 31-December-2018
+ */
+
+package myRunTimePolymorphismTests;
+
+class BaseClass {
+
+	int attribute = 1000;
+
+	void method() { System.out.println("Base Class Method"); } // Regular method
+	String returnAtribute() {return "Base Class Attribute : "+this.attribute;} // Getter Method
+}
+
+class DerivedClass extends BaseClass {
+	int attribute = 1000+1000; // Note compiler does not complain of having the same attribute
+
+	void method() { System.out.println("Derived Class Method"); } // Regular method
+	String returnAtribute() {return "Derived Class Attribute : "+this.attribute;} // Getter Method
+}
+
+public class MyRunTimePolyTest {
+
+	public static void main(String[] args) {
+		BaseClass bc = new BaseClass();
+		bc.method();
+		System.out.println("Attribute of BC : " + bc.attribute);
+
+		DerivedClass dc = new DerivedClass();
+		dc.method();
+		System.out.println("Attribute of DC : " + dc.attribute);
+
+
+		// Runtime Polymorphism in display here
+		System.out.println("\n-----'bc = dc;'-----");
+		bc = dc;
+		bc.method(); // Even though bc has a method of its own, since it is pointing to dc, it calls the derived method
+		System.out.println("\nEven though methods are called correctly b'cos of RunTime Polymorphism. Attributes are not overridden : "+ bc.attribute);
+		System.out.println("To call the attributes of DC use the proper object: " + dc.attribute);
+		System.out.println("OR create a getter method and override that. " + bc.returnAtribute());
+		
+
+	}
+
+}
